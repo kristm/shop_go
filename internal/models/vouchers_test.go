fdbf498b18b5c2e43666543d5f6b5e45287bbc68
Krist Menina <krist.menina@me.com>
2024-12-17 22:07:36 +0800

    voucher validation test

--- internal/models/vouchers_test.go
+++ internal/models/vouchers_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAddVoucher(t *testing.T) {
@@ -17,4 +18,32 @@ func TestAddVoucher(t *testing.T) {
 		Expires: expiry.Format(time.RFC3339),
 	})
 	assert.Equal(t, err, nil)
+}
+
+func TestValidateVoucher(t *testing.T) {
+	now := time.Now()
+	expired := now.AddDate(0, -1, 0)
+	err := AddVoucher(&Voucher{
+		TypeId:  2,
+		Code:    "EXPIRED",
+		Valid:   true,
+		Expires: expired.Format(time.RFC3339),
+	})
+	err = AddVoucher(&Voucher{
+		TypeId:  2,
+		Code:    "NOW",
+		Valid:   true,
+		Expires: now.Format(time.RFC3339),
+	})
+
+	validVoucher, err := ValidateVoucher("NOSHIP")
+	require.NoError(t, err)
+	invalidVoucher, err := ValidateVoucher("EXPIRED")
+	require.NoError(t, err)
+	nowVoucher, err := ValidateVoucher("NOW")
+	require.NoError(t, err)
+
+	assert.Equal(t, validVoucher, true)
+	assert.Equal(t, invalidVoucher, false)
+	assert.Equal(t, nowVoucher, true)
 }
