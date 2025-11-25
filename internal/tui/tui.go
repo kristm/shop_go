0e96fdafcd61933a94da24b788a2e399108b0fc6
Krist Menina <krist.menina@me.com>
2025-11-25 23:09:37 +0800

    style form

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -8,6 +8,8 @@ import (
 	"shop_go/internal/models"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -55,6 +57,12 @@ var (
 	//t  table.Model
 	vp viewport.Model
 
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+	noStyle             = lipgloss.NewStyle()
+
 	normal = lipgloss.Color("#EEEEEE")
 	base   = lipgloss.NewStyle().Foreground(normal)
 	subtle = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#585858"}
@@ -62,11 +70,17 @@ var (
 	background = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#CCCCCC"}
 	highlight  = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#61D4C6"}
 
+	baseStyle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#FFF7DB"))
+
 	divStyle = lipgloss.NewStyle().
 			Align(lipgloss.Left).
 			BorderStyle(lipgloss.NormalBorder()).
 			Padding(0, 2).
 			BorderForeground(lipgloss.Color("69"))
+
+	divItem = baseStyle.Foreground(lipgloss.Color("#FFD046")).Render
 
 	titleBarStyle = lipgloss.NewStyle().
 			Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
@@ -167,36 +181,95 @@ var (
 )
 
 type model struct {
-	cursor   int
-	sections []string
-	rowIndex int
+	cursor     int
+	inputs     []textinput.Model
+	focusIndex int
+	cursorMode cursor.Mode
 }
 
 func initialModel() model {
-	return model{
-		sections: []string{"Orders", "Customers", "Addresses", "Products", "Vouchers"},
-		rowIndex: 0,
+	m := model{
+		inputs:     make([]textinput.Model, 4),
+		focusIndex: 0,
+		cursorMode: cursor.CursorBlink,
 	}
+
+	var t textinput.Model
+	for i := range m.inputs {
+		t = textinput.New()
+		t.CharLimit = 32
+		t.Width = 50
+		t.Cursor.Style = cursorStyle
+
+		switch i {
+		case 0:
+			t.Placeholder = "Name"
+			t.TextStyle = focusedStyle.Foreground(lipgloss.Color("#ffffff"))
+			t.PromptStyle = focusedStyle
+			t.Focus()
+		case 1:
+			t.Placeholder = "SKU"
+		case 2:
+			t.Placeholder = "Description"
+		case 3:
+			t.Placeholder = "Price"
+		}
+
+		m.inputs[i] = t
+	}
+
+	return m
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		cmds []tea.Cmd
-	)
+	//var (
+	//	cmds []tea.Cmd
+	//)
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q", "esc":
-			cmds = append(cmds, tea.Quit)
+			return m, tea.Quit
 		}
+		cmds := make([]tea.Cmd, len(m.inputs))
+		for i := 0; i <= len(m.inputs)-1; i++ {
+			if i == m.focusIndex {
+				// Set focused state
+				cmds[i] = m.inputs[i].Focus()
+				m.inputs[i].PromptStyle = focusedStyle
+				m.inputs[i].TextStyle = focusedStyle
+				continue
+			}
+			// Remove focused state
+			m.inputs[i].Blur()
+			m.inputs[i].PromptStyle = noStyle
+			m.inputs[i].TextStyle = noStyle
+		}
+
+		return m, tea.Batch(cmds...)
+
+	}
+	// Handle character input and blinking
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
+}
+
+func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	// Only text inputs with Focus() set will respond, so it's safe to simply
+	// update all of them here without any further logic.
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
 	}
 
-	return m, tea.Batch(cmds...)
+	return tea.Batch(cmds...)
 }
 
 func (m model) View() string {
@@ -216,8 +289,24 @@ func (m model) View() string {
 			rightStatus,
 		)
 
-		form := lipgloss.JoinVertical(lipgloss.Left, divStyle.Render("HELLO\nNew form: dsfasdfasdf\nType here: sadfasdfasdf\n"))
-		body := lipgloss.JoinVertical(lipgloss.Left, bar, form)
+		div := divStyle.Render(
+			lipgloss.JoinVertical(lipgloss.Left,
+				divItem(fmt.Sprintf("%s%s", "Name:", m.inputs[0].View())),
+				divItem(fmt.Sprintf("%s%s", "SKU:", m.inputs[1].View())),
+				divItem(fmt.Sprintf("%s%s", "Description:", m.inputs[2].View())),
+				divItem(fmt.Sprintf("%s%s", "Form:", m.inputs[3].View())),
+			),
+		)
+		//var form strings.Builder
+		//for i := range m.inputs {
+		//	form.WriteString(m.inputs[i].View())
+		//	if i < len(m.inputs)-1 {
+		//		form.WriteString("\n")
+		//	}
+		//}
+
+		//form := lipgloss.JoinVertical(lipgloss.Left, div)
+		body := lipgloss.JoinVertical(lipgloss.Left, bar, div)
 
 		doc.WriteString(lipgloss.NewStyle().Width(width).Render(body) + "\n\n")
 	}
@@ -232,9 +321,8 @@ func Run() {
 		log.Printf("ERROR LOADING CONFIG")
 	}
 
-	p := tea.NewProgram(initialModel())
-	if err := p.Start(); err != nil {
-		fmt.Printf("セバエラー")
+	if _, err := tea.NewProgram(initialModel()).Run(); err != nil {
+		fmt.Printf("could not start program: %s\n", err)
 		os.Exit(1)
 	}
 }
