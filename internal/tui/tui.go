caa210b2fa76b2e06d78ded0a590880726224136
Krist Menina <krist.menina@me.com>
2025-12-01 14:00:18 +0800

    wip spinner refactor

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -14,7 +14,6 @@ import (
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/textinput"
-	"github.com/charmbracelet/bubbles/timer"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -184,15 +183,23 @@ var (
 		BorderRight(false)
 )
 
+type tickMsg struct{}
+
 type model struct {
 	cursor     int
 	form       ProductForm
 	focusIndex int
 	product    models.Product
 	response   string
-	timer      timer.Model
+	timer      int
 	spinner    spinner.Model
 	cursorMode cursor.Mode
+}
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg{}
+	})
 }
 
 func initialModel(product *models.Product) model {
@@ -205,7 +212,7 @@ func initialModel(product *models.Product) model {
 		product:    *product,
 		response:   "",
 		spinner:    s,
-		timer:      timer.New(0),
+		timer:      0,
 	}
 
 	var formModel ProductForm
@@ -273,24 +280,27 @@ func (f *ProductForm) focused() any {
 func (m model) Init() tea.Cmd {
 	return tea.Batch(
 		textinput.Blink,
-		m.timer.Init(),
+		tickCmd(),
 		m.spinner.Tick,
 	)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
-	m.timer, cmd = m.timer.Update(msg)
+	//m.timer, cmd = m.timer.Update(msg)
 
 	switch msg := msg.(type) {
-	case timer.TimeoutMsg:
-		m.response = ""
-		return m, nil
+	//case timer.TimeoutMsg:
+	//	m.response = ""
+	//	return m, nil
 
-	case timer.StartStopMsg:
-		m.timer, cmd = m.timer.Update(msg)
-		m.timer.Timeout = timeout
-		return m, cmd
+	//case timer.StartStopMsg:
+	//	m.timer, cmd = m.timer.Update(msg)
+	//	m.timer.Timeout = timeout
+	//	return m, cmd
+	case tickMsg:
+		m.timer++
+		return m, tickCmd()
 
 	case spinner.TickMsg:
 		m.spinner, cmd = m.spinner.Update(msg)
@@ -324,7 +334,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					m.response = fmt.Sprintf("Update Successful! %s", m.spinner.View())
 				}
 
-				return m, m.timer.Start()
+				return m, tickCmd()
 			}
 
 			// Cycle indexes
@@ -437,7 +447,7 @@ func (m model) View() string {
 			Padding(1).
 			Align(lipgloss.Center).
 			Foreground(lipgloss.Color("40")).
-			Render(m.response)
+			Render(m.spinner.View())
 
 		div := divStyle.Render(
 			lipgloss.JoinVertical(lipgloss.Left,
