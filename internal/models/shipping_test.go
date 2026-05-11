23d10b8be4109ecf6911ac3febd3684d109afe63
Krist Menina <krist.menina@me.com>
2026-05-11 22:53:27 +0800

    TestUpdateShippingStatus

--- internal/models/shipping_test.go
+++ internal/models/shipping_test.go
@@ -45,3 +45,34 @@ func TestGetShippingById(t *testing.T) {
 	assert.Equal(t, shipping.Phone, "8888")
 	assert.Equal(t, shipping.Notes, "X marks the spot")
 }
+
+func TestUpdateShippingStatus(t *testing.T) {
+	shippingId, err := AddShipping(&Shipping{
+		CustomerId: 1,
+		Status:     0,
+		Address:    "Malugay St.",
+		City:       "Makati",
+		Country:    "PH",
+		Zip:        "1203",
+		Phone:      "8888",
+		Notes:      "X marks the spot",
+		Fee:        90,
+	})
+
+	shipping, err := GetShippingById(shippingId)
+	require.NoError(t, err)
+	ok, _ := UpdateShippingStatus(shippingId, InTransit)
+	shipping, _ = GetShippingById(shippingId)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, shipping.Status, InTransit)
+
+	ok, _ = UpdateShippingStatus(shippingId, Shipped)
+	shipping, _ = GetShippingById(shippingId)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, shipping.Status, Shipped)
+
+	ok, _ = UpdateShippingStatus(shippingId, Delivered)
+	shipping, _ = GetShippingById(shippingId)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, shipping.Status, Delivered)
+}
